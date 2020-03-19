8c6e0838385b9bebd9118c985ca2758d7bacf43a
Pedro Vicente <pedrovicente@poli.ufrj.br>
2020-03-19 10:17:35 -0300

    Funcao checa se a conta existe

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const app = require('express')();
 const expressGraphql = require('express-graphql');
 const {buildSchema} = require('graphql');
 
+function contaExiste(conta) {
+  return providers.contas.find(item => item.conta === Number(conta))
+}
+
 const schema = buildSchema(`
   type ContaCorrente {
     conta: Int
@@ -23,44 +27,49 @@ const providers = {
 
 const resolvers = {
   saldo({conta}) {
-    if (providers.contas.find(item => item.conta === Number(conta))) {
-      return providers.contas.find(item => item.conta === Number(conta))
-    }
+    return contaExiste(conta) || {"mensagem":"Conta não existe"}
   },
   sacar({conta,valor}) {
-    const { saldo } = providers.contas.find(item => item.conta === Number(conta));
-    if (valor <= saldo) {
-      providers.contas.map(item => {
-        if (item.conta === Number(conta)) {
-          item.saldo = item.saldo - valor;
-          item.mensagem = "Saque realizado com sucesso"
-        }
-      })
-    } else {
-      providers.contas.map(item => {
-        if (item.conta === Number(conta)) {
-          item.mensagem = "Erro: Saldo insuficiente para saque";
-        }
-      });
+    if (contaExiste(conta)) {
+      const { saldo } = providers.contas.find(item => item.conta === Number(conta));
+      if (valor <= saldo) {
+        providers.contas.map(item => {
+          if (item.conta === Number(conta)) {
+            item.saldo = item.saldo - valor;
+            item.mensagem = "Saque realizado com sucesso"
+          }
+        })
+      } else {
+        providers.contas.map(item => {
+          if (item.conta === Number(conta)) {
+            item.mensagem = "Erro: Saldo insuficiente para saque";
+          }
+        });
+      }
+      return providers.contas.find(item => item.conta === Number(conta));  
     }
-    return providers.contas.find(item => item.conta === Number(conta));  
+    return {"mensagem":"Conta não existe"}
+    
   },
   depositar({conta,valor}) {
-    if (valor >= 0) {
-      providers.contas.map(item => {
-        if (item.conta === Number(conta)) {
-          item.saldo = item.saldo + valor;
-          item.mensagem = "Depósito realizado com sucesso"
-        }
-      });
-    } else {
-      providers.contas.map(item => {
-        if (item.conta === Number(conta)) {
-          item.mensagem = "Erro: Não é possível fazer depósitos de valores negativos";
-        }
-      });
+    if (contaExiste(conta)) {
+      if (valor >= 0) {
+        providers.contas.map(item => {
+          if (item.conta === Number(conta)) {
+            item.saldo = item.saldo + valor;
+            item.mensagem = "Depósito realizado com sucesso"
+          }
+        });
+      } else {
+        providers.contas.map(item => {
+          if (item.conta === Number(conta)) {
+            item.mensagem = "Erro: Não é possível fazer depósitos de valores negativos";
+          }
+        });
+      }
+      return providers.contas.find(item => item.conta === Number(conta));
     }
-    return providers.contas.find(item => item.conta === Number(conta));
+    return {"mensagem":"Conta não existe"}
   }
 };
 
